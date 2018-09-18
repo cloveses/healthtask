bb6a40c19d899ba5693848b85df2cadd9d2377ca
cloveses <cloveses@163.com>
2018-09-18 08:14:56 +0800

    m

---
+++ models/model.py
@@ -0,0 +1,70 @@
+import os
+import datetime
+from pony.orm import *
+
+db = Database()
+
+class User(db.Entity):
+    name = Required(str)
+    passwd = Required(str)
+    gender = Required(int)
+    age = Required(str)
+    token = Optional(str)
+    secure_questions = Set('SecureQuestion')
+    qestion_naires = Set('QestionNaire')
+    feed_backs = Set('FeedBack')
+    trusts = Set('Trust')
+    loginlogs = Set('LoginLog')
+    usedlocations = Set('UsedLocation')
+
+    real_name = Optional(str)
+    home_addr = Optional(str)
+    company_addr = Optional(str)
+    height = Optional(int)
+    weight = Optional(int)
+
+class Trust(db.Entity):
+    nicky_name = Required(str)
+    email = Required(str)
+    country = Required(str)
+    area = Required(str)
+    number = Required(str)
+    relationship = Required(str)
+    user = Required(User,reverse='trusts')
+
+class FeedBack(db.Entity):
+    content = Optional(str)
+    email = Optional(str)
+    score = Optional(int)
+    user = Required(User,reverse='feed_backs')
+
+class SecureQuestion(db.Entity):
+    question_id = Required(int)
+    answer = Required(str)
+    user = Required(User,reverse='secure_questions')
+
+class SsQuestion(db.Entity):
+    question = Required(str)
+    group = Required(int)
+
+class QestionNaire(db.Entity):
+    question_id = Required(str)
+    answer = Required(str)
+    user = Required(User,reverse='qestion_naires')
+
+
+class LoginLog(db.Entity):
+    date_time = Required(datetime.datetime)
+    user = Required(User,reverse='loginlogs')
+
+class UsedLocation(db.Entity):
+    longitude = Required(str)
+    latitude = Required(str)
+    address = Required(str)
+    area_type = Required(str)
+    user = Required(User,reverse='usedlocations')
+
+set_sql_debug(True)
+filename = os.path.join(os.path.abspath(os.curdir),'my.db')
+db.bind(provider='sqlite', filename=filename, create_db=True)
+db.generate_mapping(create_tables=True)
